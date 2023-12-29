cde9090feee40463bb5cc8ea51bf72d2b9fd70a4
ananastasiap <anestheziia@gmail.com>
2023-12-29 12:59:38 +0300

    feat: add Controls component / start to make timer

--- src/components/playground/playground.component.tsx
+++ src/components/playground/playground.component.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { setCurrentStep } from "./store/slices"
+import Controls from "./components/controls"
+import { INTERVAL_TIME } from "./constants"
+
 const Playground: React.FC = () => {
-  return <h1>Playground</h1>
+  const state = useAppSelector((state) => state.playground)
+  const dispatch = useAppDispatch()
+
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (isTimerActive) {
+      setInterval(() => {
+        dispatch(setCurrentStep())
+      }, INTERVAL_TIME)
+    }
+  }, [isTimerActive, dispatch])
+
+  return (
+    <div>
+      {state.currentStep}
+      <Controls
+        isTimerActive={isTimerActive}
+        setIsTimerActive={setIsTimerActive}
+      />
+    </div>
+  )
 }
 
 export default Playground
