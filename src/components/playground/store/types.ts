7f4e98c6d35761d36460526dbfe86f05d1866502
ananastasiap <anestheziia@gmail.com>
2024-01-03 20:17:15 +0300

    feat: add if our action was successful

--- src/components/playground/store/types.ts
+++ src/components/playground/store/types.ts
@@ -2,6 +2,7 @@ export interface IPlaygroundStepState {
   step: number
   currentValue: string | null
   enteredValue: string | null
+  success: boolean | null
 }
 
 export interface IPlaygroundState {
