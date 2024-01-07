cfa1bd0c9904434c3e4d881ebb60beff20338045
ananastasiap <anestheziia@gmail.com>
2024-01-07 16:45:52 +0300

    feat: add __test__

---
+++ src/components/playground/store/__test__/resetStore.spec.ts
@@ -0,0 +1,14 @@
+import playgroundReducer, {
+  initialState,
+  setCurrentStep,
+  resetStore,
+} from "../slices"
+
+describe("reducer resetStore", () => {
+  it("check resetStore", () => {
+    const setCurrentStepState = playgroundReducer(initialState, setCurrentStep)
+    const resetStoreState = playgroundReducer(setCurrentStepState, resetStore)
+
+    expect(resetStoreState).toEqual(initialState)
+  })
+})
